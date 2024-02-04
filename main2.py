c30dc5de91a0813877f048b4c034401b95179703
IssSharma <9962sidd@gmail.com>
2024-02-04 14:22:39 +0530

    Main2 file updated

---
+++ main2.py
@@ -0,0 +1,157 @@
+import speech_recognition as sr
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from easygui import *
+import os
+from PIL import Image, ImageTk
+from itertools import count
+import tkinter as tk
+import string
+#import selecting
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+from nltk.stem import PorterStemmer
+from nltk import pos_tag
+# obtain audio from the microphone
+def func():
+        r = sr.Recognizer()
+        isl_gif=['any questions', 'are you angry', 'are you busy', 'are you hungry', 'are you sick', 'be careful',
+                'can we meet tomorrow', 'did you book tickets', 'did you finish homework', 'do you go to office', 'do you have money',
+                'do you want something to drink', 'do you want tea or coffee', 'do you watch TV', 'dont worry', 'flower is beautiful',
+                'good afternoon', 'good evening', 'good morning', 'good night', 'good question', 'had your lunch', 'happy journey',
+                'hello what is your name', 'how many people are there in your family', 'i am a clerk', 'i am bore doing nothing', 
+                 'i am fine', 'i am sorry', 'i am thinking', 'i am tired', 'i dont understand anything', 'i go to a theatre', 'i love to shop',
+                'i had to say something but i forgot', 'i have headache', 'i like pink colour', 'i live in nagpur', 'lets go for lunch', 'my mother is a homemaker',
+                'my name is john', 'nice to meet you', 'no smoking please', 'open the door', 'please call me later',
+                'please clean the room', 'please give me your pen', 'please use dustbin dont throw garbage', 'please wait for sometime', 'shall I help you',
+                'shall we go together tommorow', 'sign language interpreter', 'sit down', 'stand up', 'take care', 'there was traffic jam', 'wait I am thinking',
+                'what are you doing', 'what is the problem', 'what is todays date', 'what is your father do', 'what is your job',
+                'what is your mobile number', 'what is your name', 'whats up', 'when is your interview', 'when we will go', 'where do you stay',
+                'where is the bathroom', 'where is the police station', 'you are wrong','address','agra','ahemdabad', 'all', 'april', 'assam', 'august', 'australia', 'badoda', 'banana', 'banaras', 'banglore',
+'bihar','bihar','bridge','cat', 'chandigarh', 'chennai', 'christmas', 'church', 'clinic', 'coconut', 'crocodile','dasara',
+'deaf', 'december', 'deer', 'delhi', 'dollar', 'duck', 'febuary', 'friday', 'fruits', 'glass', 'grapes', 'gujrat', 'hello',
+'hindu', 'hyderabad', 'india', 'january', 'jesus', 'job', 'july', 'july', 'karnataka', 'kerala', 'krishna', 'litre', 'mango',
+'may', 'mile', 'monday', 'mumbai', 'museum', 'muslim', 'nagpur', 'october', 'orange', 'pakistan', 'pass', 'police station',
+'post office', 'pune', 'punjab', 'rajasthan', 'ram', 'restaurant', 'saturday', 'september', 'shop', 'sleep', 'southafrica',
+'story', 'sunday', 'tamil nadu', 'temperature', 'temple', 'thursday', 'toilet', 'tomato', 'town', 'tuesday', 'usa', 'village',
+'voice', 'wednesday', 'weight','please wait for sometime','what is your mobile number','what are you doing','are you busy']
+        
+        
+        arr=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r', 's','t','u','v','w','x','y','z']
+        with sr.Microphone() as source:
+                # image   = "signlang.png"
+                # msg="HEARING IMPAIRMENT ASSISTANT"
+                # choices = ["Live Voice","All Done!"] 
+                # reply   = buttonbox(msg,image=image,choices=choices)
+                r.adjust_for_ambient_noise(source) 
+                i=0
+                while True:
+                        print("I am Listening")
+                        audio = r.listen(source)
+                        # recognize speech using Sphinx
+                        try:
+                                a=r.recognize_google(audio)
+                                a = a.lower()
+                                print('You Said: ' + a.lower())
+                                
+                                for c in string.punctuation:
+                                    a= a.replace(c,"")
+                                    
+                                #convert eng to isl grammar
+                                isl_text = convert_to_isl_grammar(a.lower())
+                                print('ISL Grammar: ' + isl_text)
+                                    
+                                if(a.lower()=='goodbye' or a.lower()=='good bye' or a.lower()=='bye'):
+                                        print("oops!Time To say good bye")
+                                        break
+                                
+                                elif(isl_text in isl_gif):
+                                    
+                                    class ImageLabel(tk.Label):
+                                            """a label that displays images, and plays them if they are gifs"""
+                                            def load(self, im):
+                                                if isinstance(im, str):
+                                                    im = Image.open(im)
+                                                self.loc = 0
+                                                self.frames = []
+
+                                                try:
+                                                    for i in count(1):
+                                                        self.frames.append(ImageTk.PhotoImage(im.copy()))
+                                                        im.seek(i)
+                                                except EOFError:
+                                                    pass
+
+                                                try:
+                                                    self.delay = im.info['duration']
+                                                except:
+                                                    self.delay = 100
+
+                                                if len(self.frames) == 1:
+                                                    self.config(image=self.frames[0])
+                                                else:
+                                                    self.next_frame()
+
+                                            def unload(self):
+                                                self.config(image=None)
+                                                self.frames = None
+
+                                            def next_frame(self):
+                                                if self.frames:
+                                                    self.loc += 1
+                                                    self.loc %= len(self.frames)
+                                                    self.config(image=self.frames[self.loc])
+                                                    self.after(self.delay, self.next_frame)
+                                    root = tk.Tk()
+                                    lbl = ImageLabel(root)
+                                    lbl.pack()
+                                    lbl.load(r'ISL_Gifs/{0}.gif'.format(isl_text))
+                                    root.mainloop()
+                                else:
+                                    for i in range(len(isl_text)):
+                                                    if(isl_text[i] in arr):
+                                            
+                                                            ImageAddress = 'letters/'+isl_text[i]+'.jpg'
+                                                            ImageItself = Image.open(ImageAddress)
+                                                            ImageNumpyFormat = np.asarray(ImageItself)
+                                                            plt.imshow(ImageNumpyFormat)
+                                                            plt.draw()
+                                                            plt.pause(0.8)
+                                                    else:
+                                                            continue
+
+                        except:
+                               print(" ")
+                        plt.close()
+                        
+def convert_to_isl_grammar(english_text):
+    # Tokenize and tag the English text
+    stop_words = set(stopwords.words('english'))
+    ps = PorterStemmer()
+    words = word_tokenize(english_text)
+    # pos_tags = pos_tag(words)
+
+    # Rearrange words to follow SOV order
+    filtered_sentence = []
+ 
+    for w in words:
+        if w not in stop_words:
+            filtered_sentence.append(ps.stem(w))
+    # sov_order = [word for (word, tag) in pos_tags]
+    sov_text = ' '.join(filtered_sentence)
+    return sov_text
+
+while 1:
+  image   = "signlang.png"
+  msg="HEARING IMPAIRMENT ASSISTANT"
+  choices = ["Live Voice","All Done!"] 
+  reply   = buttonbox(msg,image=image,choices=choices)
+  if reply ==choices[0]:
+        func()
+  if reply == choices[1]:
+        quit()
